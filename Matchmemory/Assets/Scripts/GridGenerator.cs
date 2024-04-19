2c4c7fad18108bda60616e6123c236c1eedecf21
manikandan <manikandan.gd.88@gmail.com>
2024-04-19 15:49:52 +0530

    user input fetching
    created UI to fetch the rows and columns from user

--- Matchmemory/Assets/Scripts/GridGenerator.cs
+++ Matchmemory/Assets/Scripts/GridGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
 {
+    private int rows, columns = 0;
+
     // Start is called before the first frame update
     void Start()
     {
